3b177a4741364fe8a52cf1534aa60ad4bdd09a92
LimoPhoto <lyh1671@gmail.com>
2024-11-05 19:26:10 +0900

    Update PostItemCard.js

--- themes/photo/components/PostItemCard.js
+++ themes/photo/components/PostItemCard.js
@@ -3,6 +3,7 @@ import NotionIcon from '@/components/NotionIcon' // 引入Notion图标组件，�
 import { siteConfig } from '@/lib/config' // 引入站点配置
 import { useGlobal } from '@/lib/global' // 引入全局状态钩子
 import { formatDateFmt } from '@/lib/utils/formatDate' // 引入日期格式化函数，用于格式化文章发布日期
+import { useRouter } from 'next/router' // 引入Next.js的useRouter钩子用于页面导航
 
 /**
  * 普通的博客卡片组件
@@ -11,18 +12,27 @@ import { formatDateFmt } from '@/lib/utils/formatDate' // 引入日期格式化�
 const PostItemCard = ({ post }) => {
   const { siteInfo } = useGlobal() // 使用全局状态获取站点信息
   const cover = post?.pageCoverThumbnail || siteInfo?.pageCover // 获取文章的封面图缩略图或站点默认封面图
+  const router = useRouter() // 使用useRouter钩子来实现导航
+
+  // 定义点击封面图片跳转的函数
+  const handleClick = () => {
+    router.push(post.href) // 跳转到文章详情页
+  }
 
   return (
     <div key={post.id} className='mb-6 max-w-screen-3xl'>
       {/* 卡片容器，设置底部边距和最大宽度 */}
       <div className='flex flex-col space-y-3 relative'>
-        {/* 封面图片区域 */}
-        <div className='w-full h-3/4 aspect-video overflow-hidden mb-2'>
+        {/* 封面图片区域，添加点击事件 */}
+        <div
+          className='w-full h-3/4 aspect-video overflow-hidden mb-2 cursor-pointer' // 添加cursor-pointer以显示可点击性
+          onClick={handleClick}
+        >
           <LazyImage
             alt={post?.title} // 图片替代文字为文章标题
             src={cover} // 图片来源，使用封面图或默认封面图
             style={cover ? {} : { height: '0px' }} // 如果没有封面图，设置图片高度为0
-            className='w-full h-3/4 aspect-video object-cover select-none pointer-events-none' // 设置图片宽高比例、覆盖方式，禁用选择和点击
+            className='w-full h-full object-cover select-none' // 设置图片宽高比例、覆盖方式，禁用选择
           />
         </div>
 
