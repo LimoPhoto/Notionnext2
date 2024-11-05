0a9d4c9d735d8494c01f8c6d3f8766b513761a01
LimoPhoto <lyh1671@gmail.com>
2024-11-06 01:41:03 +0900

    Update SearchInput.js

--- themes/photo/components/SearchInput.js
+++ themes/photo/components/SearchInput.js
@@ -8,6 +8,7 @@ const SearchInput = ({ currentTag, keyword, cRef }) => {
   const { locale } = useGlobal()
   const router = useRouter()
   const searchInputRef = useRef(null)
+
   useImperativeHandle(cRef, () => {
     return {
       focus: () => {
@@ -15,6 +16,7 @@ const SearchInput = ({ currentTag, keyword, cRef }) => {
       }
     }
   })
+
   const handleSearch = () => {
     const key = searchInputRef.current.value
     if (key && key !== '') {
@@ -22,10 +24,10 @@ const SearchInput = ({ currentTag, keyword, cRef }) => {
         console.log('搜索', key)
       })
     } else {
-      router.push({ pathname: '/' }).then(r => {
-      })
+      router.push({ pathname: '/' }).then(r => {})
     }
   }
+
   const handleKeyUp = (e) => {
     if (e.keyCode === 13) { // 回车
       handleSearch(searchInputRef.current.value)
@@ -33,10 +35,12 @@ const SearchInput = ({ currentTag, keyword, cRef }) => {
       cleanSearch()
     }
   }
+
   const cleanSearch = () => {
     searchInputRef.current.value = ''
     setShowClean(false)
   }
+
   function lockSearchInput () {
     lock = true
   }
@@ -44,44 +48,19 @@ const SearchInput = ({ currentTag, keyword, cRef }) => {
   function unLockSearchInput () {
     lock = false
   }
+
   const [showClean, setShowClean] = useState(false)
+
   const updateSearchKey = (val) => {
     if (lock) {
       return
     }
     searchInputRef.current.value = val
-    if (val) {
-      setShowClean(true)
-    } else {
-      setShowClean(false)
-    }
+    setShowClean(!!val)
   }
 
-  return <section className='flex w-full bg-gray-100'>
-  <input
-    ref={searchInputRef}
-    type='text'
-    placeholder={currentTag ? `${locale.SEARCH.TAGS} #${currentTag}` : `${locale.SEARCH.ARTICLES}`}
-    className={'outline-none w-full text-sm pl-4 transition focus:shadow-lg font-light leading-10 text-black bg-gray-100 dark:bg-gray-900 dark:text-white'}
-    onKeyUp={handleKeyUp}
-    onCompositionStart={lockSearchInput}
-    onCompositionUpdate={lockSearchInput}
-    onCompositionEnd={unLockSearchInput}
-    onChange={e => updateSearchKey(e.target.value)}
-    defaultValue={keyword || ''}
-  />
-
-  <div className='-ml-8 cursor-pointer float-right items-center justify-center py-2'
-    onClick={handleSearch}>
-      <i className={'hover:text-black transform duration-200  text-gray-500 cursor-pointer fas fa-search'} />
-  </div>
-
-  {(showClean &&
-    <div className='-ml-12 cursor-pointer dark:bg-gray-600 dark:hover:bg-gray-800 float-right items-center justify-center py-2'>
-      <i className='hover:text-black transform duration-200 text-gray-400 cursor-pointer fas fa-times' onClick={cleanSearch} />
-    </div>
-    )}
-</section>
+  // 隐藏搜索框
+  return null
 }
 
 export default SearchInput
