6fb4c091eb7225c982d2e184a287afc21566eb17
LimoPhoto <lyh1671@gmail.com>
2024-11-06 14:27:57 +0900

    Update Header.js

--- themes/photo/components/Header.js
+++ themes/photo/components/Header.js
@@ -11,140 +11,76 @@ import { MenuItemDrop } from './MenuItemDrop'
 
 /**
  * 网站顶部
- * @returns
+ * @returns {JSX.Element} - 页头组件
  */
 export const Header = props => {
-  const { collapseRef, searchModal } = usePhotoGlobal()
+  const { collapseRef } = usePhotoGlobal() // 移除 searchModal
   const router = useRouter()
   const { customNav, customMenu } = props
   const { locale } = useGlobal()
   const [isOpen, setIsOpen] = useState(false)
-  const [showSearch, setShowSearch] = useState(false)
   const toggleMenuOpen = () => {
     setIsOpen(!isOpen)
   }
+
+  // 默认导航链接设置，移除搜索项
   let links = [
     {
       id: 1,
       icon: 'fa-solid fa-house',
-      name: locale.NAV.INDEX,
+      name: locale.NAV.INDEX, // 首页导航项
       href: '/',
       show: siteConfig('MOVIE_MENU_INDEX', null, CONFIG)
     },
     {
-      id: 2,
-      icon: 'fas fa-search',
-      name: locale.NAV.SEARCH,
-      href: '/search',
-      show: siteConfig('MOVIE_MENU_SEARCH', null, CONFIG)
-    },
-    {
       id: 3,
       icon: 'fas fa-archive',
-      name: locale.NAV.ARCHIVE,
+      name: locale.NAV.ARCHIVE, // 归档导航项
       href: '/archive',
       show: siteConfig('MOVIE_MENU_ARCHIVE', null, CONFIG)
     }
-    // { icon: 'fas fa-folder', name: locale.COMMON.CATEGORY, href: '/category', show: siteConfig('MENU_CATEGORY', null, CONFIG) },
-    // { icon: 'fas fa-tag', name: locale.COMMON.TAGS, href: '/tag', show: siteConfig('MENU_TAG', null, CONFIG) }
   ]
 
   if (customNav) {
     links = links.concat(customNav)
   }
 
+  // 重置链接的ID以确保唯一性
   for (let i = 0; i < links.length; i++) {
     if (links[i].id !== i) {
       links[i].id = i
     }
   }
 
-  // 如果 开启自定义菜单，则覆盖Page生成的菜单
+  // 如果启用自定义菜单，则覆盖Page生成的默认菜单
   if (siteConfig('CUSTOM_MENU')) {
     links = customMenu
   }
 
-  // 展示搜索框
-  const toggleShowSearchInput = () => {
-    if (siteConfig('ALGOLIA_APP_ID')) {
-      searchModal.current.openSearch()
-    } else {
-      setShowSearch(!showSearch)
-    }
-  }
-
-  useEffect(() => {
-    if (showSearch) {
-      setTimeout(() => {
-        document.getElementById('search').focus()
-      }, 100)
-    }
-  }, [showSearch])
-
-  const onKeyUp = e => {
-    if (e.keyCode === 13) {
-      const search = document.getElementById('search').value
-      if (search) {
-        router.push({ pathname: '/search/' + search })
-      }
-    }
-  }
-
-  const handleSearch = () => {
-    const search = document.getElementById('search').value
-    if (search) {
-      router.push({ pathname: '/search/' + search })
-    }
-  }
-
   return (
     <>
+      {/* 页头容器 */}
       <header className='w-full px-8 h-20 z-20 flex lg:flex-row md:flex-col justify-between items-center'>
         {/* 左侧Logo */}
         <Link
           href='/'
           className='logo whitespace-nowrap text-2xl md:text-3xl font-bold text-gray-dark no-underline flex items-center'>
-          {siteConfig('TITLE')}
+          {siteConfig('TITLE')} {/* 显示站点标题 */}
         </Link>
 
+        {/* 右侧菜单 */}
         <div className='md:w-auto text-center flex space-x-2'>
-          {/* 右侧菜单 */}
+          {/* 右侧菜单项 */}
           <>
             <nav
               id='nav-mobile'
               className='leading-8 justify-center w-full hidden md:flex'>
+              {/* 显示每个菜单链接 */}
               {links?.map(
                 (link, index) =>
-                  link && link.show && <MenuItemDrop key={index} link={link} />
+                  link && link.show && <MenuItemDrop key={index} link={link} /> // 条件渲染菜单项
               )}
             </nav>
-
-            <div
-              onClick={toggleShowSearchInput}
-              className='flex items-center cursor-pointer'>
-              <i className='fas fa-search dark:text-white'></i>
-            </div>
-
-            <div
-              className={`${showSearch ? 'top-16 visible opacity-100' : 'top-10 invisible opacity-0'} duration-200 transition-all max-w-md absolute  w-80 right-4 p-2 flex flex-col gap-2`}>
-              <input
-                autoFocus
-                id='search'
-                onClick={toggleShowSearchInput}
-                onKeyUp={onKeyUp}
-                className='float-left w-full outline-none h-full p-2 rounded text-black bg-gray-100'
-                aria-label='Submit search'
-                type='search'
-                name='s'
-                autoComplete='off'
-                placeholder='Type then hit enter to search...'
-              />
-              <button
-                onClick={handleSearch}
-                className='w-full bg-[#383838] rounded py-2'>
-                {locale.COMMON.SEARCH} 搜索
-              </button>
-            </div>
 
             {/* 移动端按钮 */}
             <div className='md:hidden'>
@@ -160,20 +96,21 @@ export const Header = props => {
         </div>
       </header>
 
+      {/* 移动端菜单折叠内容 */}
       <Collapse
         className='block md:hidden'
         collapseRef={collapseRef}
         type='vertical'
         isOpen={isOpen}>
-        {/* 移动端菜单 */}
         <menu id='nav-menu-mobile' className='my-auto justify-start'>
+          {/* 显示每个菜单链接 */}
           {links?.map(
             (link, index) =>
               link &&
               link.show && (
                 <MenuItemCollapse
                   onHeightChange={param =>
-                    collapseRef.current?.updateCollapseHeight(param)
+                    collapseRef.current?.updateCollapseHeight(param) // 更新折叠高度
                   }
                   key={index}
                   link={link}
