1b72e85e41db29ba6c388485826da76865e86c0f
LimoPhoto <lyh1671@gmail.com>
2024-11-06 12:39:54 +0900

    Update Footer.js

--- themes/photo/components/Footer.js
+++ themes/photo/components/Footer.js
@@ -1,12 +1,47 @@
+import { BeiAnGongAn } from '@/components/BeiAnGongAn'
+import { siteConfig } from '@/lib/config'
 /**
- * 页脚组件
- * @param {*} props - 传递的属性
- * @returns {JSX.Element} 页脚内容
+ * 页脚
+ * @param {*} props
+ * @returns
  */
 export const Footer = props => {
+  const d = new Date()
+  const currentYear = d.getFullYear()
+  const since = siteConfig('SINCE')
+  const copyrightDate =
+    parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
+
   return (
-    <footer className='z-10 relative w-full bg-white px-6 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray'>
-      {/* 页脚内容已隐藏 */}
+    <footer className='z-10 relative w-full bg-white px-6 border-t dark:border-hexo-black-gray dark:bg-hexo-black-gray '>
+      {/* 移除 DarkModeButton */}
+
+      <div className='container mx-auto max-w-4xl py-6 md:flex flex-wrap md:flex-no-wrap md:justify-between items-center text-sm'>
+        <div className='text-center'>
+          &copy;{`${copyrightDate}`} {siteConfig('AUTHOR')}. All rights
+          reserved.
+        </div>
+        <div className='md:p-0 text-center md:text-right text-xs'>
+          {/* 右侧链接 */}
+          {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
+          {siteConfig('BEI_AN') && (
+            <a
+              href='https://beian.miit.gov.cn/'
+              className='text-black dark:text-gray-200 no-underline hover:underline ml-4'>
+              {siteConfig('BEI_AN')}
+            </a>
+          )}
+          <BeiAnGongAn />
+          <span className='dark:text-gray-200 no-underline ml-4'>
+            Powered by
+            <a
+              href='https://github.com/tangly1024/NotionNext'
+              className=' hover:underline'>
+              NotionNext {siteConfig('VERSION')}
+            </a>
+          </span>
+        </div>
+      </div>
     </footer>
   )
 }
