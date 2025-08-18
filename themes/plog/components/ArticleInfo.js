c744fea022d8eeaf1828fde605698598d42e5f47
LimoPhoto <lyh1671@gmail.com>
2025-08-18 01:09:05 -0400

    Update ArticleInfo.js

--- themes/plog/components/ArticleInfo.js
+++ themes/plog/components/ArticleInfo.js
@@ -21,20 +21,25 @@ export const ArticleInfo = (props) => {
             <nav className="flex mt-7 items-start text-gray-500 dark:text-gray-400">
             <div className="flex mb-4">
               <a href={siteConfig('CONTACT_GITHUB') || '#'} className="flex">
-                <Image
+               {/*  
+              <Image
                   alt={siteConfig('AUTHOR')}
                   width={24}
                   height={24}
                   src={`https://gravatar.com/avatar/${emailHash}`}
                   className="rounded-full"
                 />
+                */}
                 <p className="ml-2 md:block">{siteConfig('AUTHOR')}</p>
               </a>
               <span className="block">&nbsp;/&nbsp;</span>
             </div>
+              {/*
             <div className="mr-2 mb-4 md:ml-0">
               {post?.publishDay}
             </div>
+            */}
+
             {post?.tags && (
               <div className="flex flex-nowrap max-w-full overflow-x-auto article-tags">
                 {post?.tags.map(tag => (
@@ -42,11 +47,13 @@ export const ArticleInfo = (props) => {
                 ))}
               </div>
             )}
+              {/*
             <span className="hidden busuanzi_container_page_pv mr-2">
                     <i className='mr-1 fas fa-eye' />
                     &nbsp;
                     <span className="mr-2 busuanzi_value_page_pv" />
                 </span>
+                */}
              </nav>
             </>}
 
